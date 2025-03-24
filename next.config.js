83c6bee2b3624a1107a463474b41dca11b884598
Tony Davis <tony@flynectair.com>
2025-03-24 18:23:46 -0400

    Add cache-busting build ID

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
       },
     ],
   },
+  generateBuildId: async () => {
+    // This will generate a new build ID on each build, forcing a clean cache
+    return `build-${Date.now()}`
+  },
 }
 
 module.exports = nextConfig
